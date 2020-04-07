05e9f94bf5e3fb9db43e638e470792498249c389
Jay Wongkot <onzonstudios@gmail.com>
2020-04-07 16:28:42 -0300

    Database setup and models registation for blog app

--- jwportfolio/blog/models.py
+++ jwportfolio/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+# Import user model from Django library
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    # [1] models.DateTimeField(auto_now=true), to update a blog post base on a current user login timzone which is not consistency
+    #     for our blog post if we have million useres from over different timezone. This would be great for a last modified field.
+    # [2] models.DateTimeField(auto_now_add=true)  This would set the date posted to the current date time only when this object is created.
+    #     But with the auto_now_add=true, you can't ever update the value of the date posted so it will have to keep the exact date time
+    #     of when the post was created.
+    # [3] models.DateTimeField(default=timezone.now) This option will do the same with option no.2 but you have the option of changing the date.
+    #     We don't put a parentheses after default=timzone.now eventhrough it is a function because we don't want it to excute.
+    #     We just a default value of this function.
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- jwportfolio/blog/views.py
+++ jwportfolio/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+# From a models.py in the same directory, we import a Post class
+from .models import Post
 # from django.http import HttpResponse # Import HttpResponse only need when we want to return http resquest function
 
 # We created dummy posts to render in home view and to loop through in home.html template
@@ -20,8 +22,13 @@ posts = [
 
 def home(request):
     # we created a dictionary to pass a data that we pretended to call and response from a database
+    # context = {
+    #     'posts': posts  # We created a key and passed in a dictionary that we created at the top
+    # }
+
     context = {
-        'posts': posts  # We created a key and passed in a dictionary that we created at the top
+        # We will fetch a data from a database instead of a dummy posts
+        'posts': Post.objects.all()
     }
     # return HttpResponse('<h1>Blog Home</h1>')
     # We passed in a third argument as 'context' to render a dummy post
