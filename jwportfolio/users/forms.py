8dfb30b31da7d3e6fd67464c27200557124994fa
Jay Wongkot <onzonstudios@gmail.com>
2020-04-13 11:00:11 -0300

    Created Update User Profile and Resize Profile Image

--- jwportfolio/users/forms.py
+++ jwportfolio/users/forms.py
@@ -1,11 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
 
 class UserRegisterForm(UserCreationForm):
     # Here, we will specify a new field that we need to add to our form
-    email = forms.EmailField() # By default EmailField(true) is set required to true but 
-                               # if your want it to not required you can specify forms.EmailField(False)
-    class Meta: # This Meta class will save a new username in our User model table in database
-        model = User # whenever this form validated, it will create a new user.
+    email = forms.EmailField()  # By default EmailField(true) is set required to true but
+    # if your want it to not required you can specify forms.EmailField(False)
+
+    class Meta:  # This Meta class will save a new username in our User model table in database
+        # whenever this form validated, it will create a new user.
+        model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- jwportfolio/users/models.py
+++ jwportfolio/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# Here we'll import Image library from Pillow package to resize an image
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +10,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # Here we will resize a large image to a smaller size to less than 300 pixels
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
